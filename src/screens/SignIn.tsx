aa261d13ce8bc32e14199b226b2dcf644e0c9c29
Matheus <matheusim27@hotmail.com>
2023-12-19 20:45:19 -0300

    feat: input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { VStack, Image, Text, Center, Heading } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
+import Input from '@components/Input'
 
 export default function SignIn() {
   return (
@@ -23,6 +24,8 @@ export default function SignIn() {
         <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
           Acesse sua conta
         </Heading>
+        <Input placeholder='E-mail' keyboardType='email-address' autoCapitalize='none' />
+        <Input placeholder='Senha' secureTextEntry />
       </Center>
     </VStack>
   )
