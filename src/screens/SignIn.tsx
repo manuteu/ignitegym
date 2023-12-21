607708e3c942c00a04f5ecd3939f9f5301325878
Matheus <matheusim27@hotmail.com>
2023-12-21 00:31:30 -0300

    feat: navigation theme and navigate screens

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { VStack, Image, Text, Center, Heading, ScrollView, Box } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import Input from '@components/Input'
 import Button from '@components/Button'
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
+import { useNavigation } from '@react-navigation/native'
 
 export default function SignIn() {
+
+  const { navigate } = useNavigation<AuthNavigatorRoutesProps>()
+
+  const handleNewAccount = () => {
+    navigate('signUp')
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
       <VStack
-        flex={1}
         bgColor={'gray.700'}
         pb={16}
+        flex={1}
       >
         <Image
           source={BackgroundImg}
@@ -22,44 +31,46 @@ export default function SignIn() {
           resizeMode='contain'
           position="absolute"
         />
-        <Center my={24}>
-          <LogoSvg />
-          <Text color='gray.100' fontSize='sm'>
-            Treine sua mente e o seu corpo
-          </Text>
-        </Center>
-        <Center>
-          <Heading
-            color='gray.100'
-            fontSize='xl'
-            mb={6}
-            fontFamily='heading'
-          >
-            Acesse sua conta
-          </Heading>
-          <Input
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none'
-          />
-          <Input
-            placeholder='Senha'
-            secureTextEntry
-          />
-          <Button title='Acessar' />
-        </Center>
-        <Center>
-          <Text
-            color='gray.100'
-            fontSize='sm'
-            mb={3}
-            fontFamily='body'
-          >
-            Ainda não tem acesso?
-          </Text>
+        <Box flex={1} justifyContent='space-around' px={10}>
+          <Center mt={10}>
+            <LogoSvg />
+            <Text color='gray.100' fontSize='sm'>
+              Treine sua mente e o seu corpo
+            </Text>
+          </Center>
+          <Center>
+            <Heading
+              color='gray.100'
+              fontSize='xl'
+              mb={6}
+              fontFamily='heading'
+            >
+              Acesse sua conta
+            </Heading>
+            <Input
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCapitalize='none'
+            />
+            <Input
+              placeholder='Senha'
+              secureTextEntry
+            />
+            <Button title='Acessar' />
+          </Center>
+          <Center>
+            <Text
+              color='gray.100'
+              fontSize='sm'
+              mb={3}
+              fontFamily='body'
+            >
+              Ainda não tem acesso?
+            </Text>
 
-          <Button title='Criar conta' variant='outline' />
-        </Center>
+            <Button title='Criar conta' variant='outline' onPress={handleNewAccount} />
+          </Center>
+        </Box>
       </VStack>
     </ScrollView>
   )
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { VStack, Image, Text, Center, Heading, ScrollView, Box } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import Input from '@components/Input'
 import Button from '@components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 export default function SignUp() {
+
+  const { goBack } = useNavigation()
+
+  const handleGoBack = () => {
+    goBack()
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -18,53 +26,55 @@ export default function SignUp() {
       >
         <Image
           source={BackgroundImg}
+          defaultSource={BackgroundImg}
           alt='Pessoas treinando'
           resizeMode='contain'
           position="absolute"
         />
+        <Box flex={1} justifyContent='space-around' px={10}>
+          <Center mt={10}>
+            <LogoSvg />
+            <Text
+              color='gray.100'
+              fontSize='sm'
+            >
+              Treine sua mente e o seu corpo
+            </Text>
+          </Center>
 
-        <Center my={24}>
-          <LogoSvg />
-          <Text
-            color='gray.100'
-            fontSize='sm'
-          >
-            Treine sua mente e o seu corpo
-          </Text>
-        </Center>
+          <Center>
+            <Heading
+              color='gray.100'
+              fontSize='xl'
+              mb={6}
+              fontFamily='heading'
+            >
+              Crie sua conta
+            </Heading>
 
-        <Center>
-          <Heading
-            color='gray.100'
-            fontSize='xl'
-            mb={6}
-            fontFamily='heading'
-          >
-            Crie sua conta
-          </Heading>
+            <Input
+              placeholder='Nome'
+            />
+            <Input
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCapitalize='none'
+            />
+            <Input
+              placeholder='Senha'
+              secureTextEntry
+            />
+            <Button
+              title='Criar e acessar'
+            />
+          </Center>
 
-          <Input
-            placeholder='Nome'
-          />
-          <Input
-            placeholder='E-mail'
-            keyboardType='email-address'
-            autoCapitalize='none'
-          />
-          <Input
-            placeholder='Senha'
-            secureTextEntry
-          />
           <Button
-            title='Criar e acessar'
+            onPress={handleGoBack}
+            title='Voltar par o login'
+            variant='outline'
           />
-        </Center>
-
-        <Button
-          mt={24}
-          title='Voltar par o login'
-          variant='outline'
-        />
+        </Box>
       </VStack>
     </ScrollView>
   )
