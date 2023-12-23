d919f2c7e635988debddfeff4e1128e2c7e9c4a5
Matheus <matheusim27@hotmail.com>
2023-12-23 14:19:24 -0300

    feat: history interface

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,18 @@
+import { Center, Heading } from 'native-base'
+import React from 'react'
+
+type Props = {
+  title: string;
+}
+
+export default function ScreenHeader({ title }: Props) {
+  return (
+    <Center bg='gray.600' pb={6} pt={16}>
+      <Heading
+        color='gray.100' fontSize='xl'
+      >
+        {title}
+      </Heading>
+    </Center>
+  )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import { Center, Text } from 'native-base'
+import React, { useState } from 'react'
+import { Heading, SectionList, Text } from 'native-base'
+import { VStack } from 'native-base'
+import ScreenHeader from '@components/ScreenHeader'
+import HistoryCard from '@components/HistoryCard'
 
 export default function History() {
+  const [exercises, setExercises] = useState([
+    {
+      title: '22.22.22',
+      data: ['maco', 'saco', 'daco']
+    },
+    {
+      title: '22.22.23',
+      data: ['maco', 'saco', 'daco']
+    }
+  ])
   return (
-    <Center flex={1}>
-      <Text  color='white'>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title='Histório de Exercícios' />
+      <SectionList
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+        contentContainerStyle={[].length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color='gray.100' textAlign='center'>
+            Bora treinar frango!
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+    </VStack>
   )
 }
