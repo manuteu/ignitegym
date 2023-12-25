fd8bd93fa69e44c63efa2b06f3f73c7c6891db1c
Matheus <matheusim27@hotmail.com>
2023-12-25 18:08:01 -0300

    feat: image picker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,21 +1,64 @@
 import React, { useState } from 'react'
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base'
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base'
 import ScreenHeader from '@components/ScreenHeader'
 import UserPhoto from '@components/UserPhoto'
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Alert } from 'react-native';
 import Input from '@components/Input';
 import Button from '@components/Button';
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system';
 
 const PHOTO_SIZE = 33;
 
 export default function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+  const [userPhoto, setUserPhoto] = useState('https://github.com/manuteu.png')
 
+  const toast = useToast()
+
+  const handleUserPhotoSelect = async () => {
+    setPhotoIsLoading(true)
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+      console.log(photoSelected);
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      // Utiliza o FileSystem para verificar o tamanho da imagem, já que o Image picker não possui essa função
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+
+        // Converte para 5 MB
+        if (photoInfo.exists && photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          // Snackbar, notifica o usuário com uma mensagem
+          return toast.show({
+            title: 'Essa imagem é muito grande. Escolha uma de até 5MB.',
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
+  }
 
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
-      <ScrollView contentContainerStyle={{paddingBottom: 36}}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading ? (
             <Skeleton
@@ -28,12 +71,12 @@ export default function Profile() {
           ) : (
             <UserPhoto
               size={PHOTO_SIZE}
-              source={{ uri: 'https://github.com/manuteu.png' }}
+              source={{ uri: userPhoto }}
               alt='Foto do usuário'
             />
           )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color='green.500' fontWeight='bold' fontSize='md' mt={2} mb={8}>
               Alterar Foto
             </Text>
