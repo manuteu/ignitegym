9a119104e9f9a90255ad7c41ff69d0d5a1dae847
Matheus <matheusim27@hotmail.com>
2024-04-16 09:54:00 -0300

    expo pre build

--- App.tsx
+++ App.tsx
@@ -5,6 +5,10 @@ import Loading from '@components/Loading';
 import { THEME } from './src/theme';
 import { Routes } from '@routes/index';
 import { AuthContextProvider } from '@contexts/AuthContext';
+import { OneSignal } from 'react-native-onesignal'
+
+OneSignal.initialize('41ec8ad1-8005-4994-91f5-56a0981d5236')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
