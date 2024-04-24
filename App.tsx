7414d4536e61ad99e28a782cf236f37c90f96cf0
Matheus <matheusim27@hotmail.com>
2024-04-24 09:25:09 -0300

    feat: notification actions

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@ import Loading from '@components/Loading';
 import { THEME } from './src/theme';
 import { Routes } from '@routes/index';
 import { AuthContextProvider } from '@contexts/AuthContext';
-import { OneSignal } from 'react-native-onesignal'
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal'
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
 
 OneSignal.initialize('41ec8ad1-8005-4994-91f5-56a0981d5236')
 OneSignal.Notifications.requestPermission(true)
@@ -15,6 +16,26 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate({ user_email: 'mathtechn@gmail.com', user_name: 'Matheus' })
+
+  useEffect(() => {
+    const handleNotificationClick = (event: NotificationClickEvent): void => {
+      const { actionId } = event.result
+
+      if (actionId === '1') {
+        return console.log('Ver Todos')
+      }
+      if (actionId === '2') {
+        return console.log('Ver Exercício')
+      }
+    }
+
+    OneSignal.Notifications.addEventListener('click', handleNotificationClick)
+
+    return () => {
+      OneSignal.Notifications.removeEventListener('click', handleNotificationClick)
+    }
+  }, [])
+
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
